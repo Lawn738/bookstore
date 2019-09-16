8acce25e7bc05398749967c0b239405a5481c486
Lawn738 <44837018+Lawn738@users.noreply.github.com>
2019-09-16 03:05:09 +0300

    modified:   pom.xml 	modified:   src/main/java/hh/swd22/bookstore/webcontroller/BookController.java 	new file:   src/main/resources/templates/index.html

--- src/main/java/hh/swd22/bookstore/webcontroller/BookController.java
+++ src/main/java/hh/swd22/bookstore/webcontroller/BookController.java
@@ -1,17 +1,34 @@
 package hh.swd22.bookstore.webcontroller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.Entity;
+
+import hh.swd22.bookstore.domain.Book;
+
 @Controller
-@ResponseBody
 public class BookController {
 	
-	@RequestMapping("/index")
-	public String hello(@RequestParam(name="place") String place,
-			@RequestParam(name="name") String name) {
-		return "welcome to the " + place + " " + name;
+	public List<Book> books;
+	
+	public BookController() {
+		
+		this.books = new ArrayList<Book>();
+		this.books.add(new Book("harry potter, viisastenkivi", "J.K Rowling", "2000", "0000000001", 25));
+		this.books.add(new Book("harry potter, salaisuuksien kammio", "J.K Rowling", "2000", "0000000002", 25));
+	}
+	
+	@GetMapping(value = "/")
+	public String home(Model model) {
+		model.addAttribute("list", books);
+		return "index";
 	}
 }
